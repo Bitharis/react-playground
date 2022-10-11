8ffb49593c4813c17e37f33331c743a7c8fcccb9
panos <bitharis@gmail.com>
2022-10-11 20:19:08 +0200

    add meals

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton>Cart</HeaderCartButton>
+      </header>
+      
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="table full of delicious foods" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
