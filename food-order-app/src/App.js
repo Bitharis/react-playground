04b270a36eddff835329a4c1fd730839d0ed6e05
panos <bitharis@gmail.com>
2022-10-15 09:41:02 +0200

    add Close functionality on Cart modal

--- food-order-app/src/App.js
+++ food-order-app/src/App.js
@@ -1,13 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [isCartVisible, setIsCartVisible] = useState(false);
+
+  const showCart = () => {
+    setIsCartVisible(true);
+  };
+
+  const hideCart = () => {
+    setIsCartVisible(false);
+  };
+
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      {isCartVisible && <Cart onClose={hideCart}/>}
+      <Header onShowCart={showCart}/>
       <main>
         <Meals />
       </main>
